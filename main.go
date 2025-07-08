5898ac17585f3f45ce09c165acc7f1a12e600b1b
Paul Jang <happyness980817@gmail.com>
2025-07-08 09:39:08 +0900

    maps

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	arrayOfNames := [5]string{"a", "b", "c", "d", "e"} // array
-	fmt.Println(arrayOfNames)
-
-	sliceOfNames := []string{"a","b","c"} // slice
-	sliceOfNames = append(sliceOfNames,"d") // Append returns the updated slice.
-	fmt.Println(sliceOfNames)
+	nico := map[string]string{"name":"nico","age":"12"} // key, value
+	for _, value := range nico {
+		fmt.Println(value)
+	}
 }

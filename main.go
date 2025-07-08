c6a5f644ae5101daf38064464ec2b655d2bc7de6
Paul Jang <happyness980817@gmail.com>
2025-07-08 09:53:25 +0900

    struct

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	nico := map[string]string{"name":"nico","age":"12"} // key, value
-	for _, value := range nico {
-		fmt.Println(value)
-	}
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name:"nico", age:18, favFood: favFood}
+	fmt.Println(nico)
 }

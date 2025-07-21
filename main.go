5545478a138d2eec02bcce7776b4cca047a6f38e
Paul Jang <happyness980817@gmail.com>
2025-07-21 16:55:38 +0900

    methods practice

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
+	err := account.Withdraw(2)
 	if err != nil {
 		// log.Fatalln(err)
 		fmt.Println(err)
 	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
+	// fmt.Println(account.Balance(), account.Owner())
 }

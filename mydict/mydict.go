62c50a46c57530c3b2780319e6ab2c0f21ed567b
Paul Jang <happyness980817@gmail.com>
2025-07-23 16:08:23 +0900

    dictionary (2)

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,11 +5,26 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("value not found")
+var errWordExists = errors.New("existing word")
 
 func (d Dictionary) Search(word string) (string, error) {
-	value, exists := d[word]
+	value, exists := d[word] // map 의 key 를 호출하면 두 종류의 값을 반환. 
+	// 값, ok 두 종류의 값, ok 는 값이 존재하는지 알려주는 boolean
 	if exists {
 		return value, nil
 	}
-	return "", errNotFound
+	return "", errNotFound // can use methods inside the types in Go
+} 
+// we don't need the * on the receiver
+// because maps in Go are automatically using *
+
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }

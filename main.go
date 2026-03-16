4e1ffe3087e965da41ab518795d2b0b5e58691b9
Paul Jang <happyness980817@gmail.com>
2026-03-16 13:57:31 +0900

    #3.4 Channels Recap

--- main.go
+++ main.go
@@ -9,17 +9,17 @@ import (
 // channel: goroutine 과 main 함수 사이에 (또는 goroutine 과 goroutine 사이 등등...) 정보를 전달하기 위한 방법
 
 func main() {
-	c := make(chan bool)
-	people := [2]string{"john","jane"}
+	c := make(chan string)
+	people := [5]string{"john","jane","dal","japanguy","larry"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
 }
 
-func isSexy(person string, c chan bool) {
+func isSexy(person string, c chan string) {
 	time.Sleep(time.Second * 2)
-	fmt.Println(person)
-	c <- true
+	c <- person + " is sexy"
 }
